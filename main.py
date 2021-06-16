f9cfef9d6dc3092ffed3f86b9ca508b27206999f
zunayedsyed <71093884+zunayedsyed@users.noreply.github.com>
2021-06-16 18:02:17 +0600

    even number or odd number in list
    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# even number or odd number in list
+# create a function 
+def list():
+	x=[1,2,3]
+	for y in x:
+		if y%2==0:
+			print(y,"even")
+		elif y%2==1:
+			print(y,"odd")
+# function call 
+list()
+		
